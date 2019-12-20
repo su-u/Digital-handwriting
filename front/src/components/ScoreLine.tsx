9470b2eb6df124badbdd08babb6e0913874aa135
su- <aaaiiiuuu35@gmail.com>
2019-12-21 08:22:28 +0900

    Create ScoreLine.tsx

---
+++ front/src/components/ScoreLine.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props{
+    img: string;
+    text: string;
+    score: string;
+}
+
+const ScoreLine: React.FC<Props> = (props:Props) => {
+    return (
+        <>
+        </>
+    )
+};
+
+export default ScoreLine;
