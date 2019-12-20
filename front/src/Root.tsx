c776ec72c3bc1d1de3705ba56ef7cf8158a5dbf3
su- <aaaiiiuuu35@gmail.com>
2019-12-21 08:22:14 +0900

    整理

--- front/src/Root.tsx
+++ front/src/Root.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, HashRouter } from "react-router-dom";
 import GlobalStyle from "@/components/GlobalStyle";
-import App from "@/App";
+import App from "@/container/App";
 import HeaderComponent from "@/components/Header";
 
 export default () => (
