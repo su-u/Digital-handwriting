a623907f6ce7c43f091a3bfc270d62f78ac2e37d
su- <aaaiiiuuu35@gmail.com>
2019-12-21 08:26:45 +0900

    Update QuestionText.tsx

--- front/src/components/QuestionText.tsx
+++ front/src/components/QuestionText.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const QuestionText = () => {
     return(
         <>
+            <QuestionWrapeer>
+                <QuestionNumber>
+                </QuestionNumber>
+            </QuestionWrapeer>
         </>
     );
 };
+
+const QuestionWrapeer = styled.div`
+
+`;
+
+const QuestionNumber = styled.div``;
 
 export default QuestionText;
